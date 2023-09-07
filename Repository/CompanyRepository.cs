16ca4c7be08b809b1a642d8c49f375897db5acff
ahmed2016170053 <ahmed2016170053@cis.asu.edu.eg>
2023-09-07 12:03:09 +0300

    add async methods

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -20,10 +20,9 @@ namespace Repository
             .OrderBy(c => c.Name)
             .ToListAsync();
 
-        public async Task<Company> GetCompany(Guid companyId, bool trackChanges) =>
+        public async Task<Company> GetCompanyAsync(Guid companyId, bool trackChanges) =>
             await FindByCondition(c => c.Id.Equals(companyId), trackChanges)
             .SingleOrDefaultAsync();
-
 
         public void CreateCompany(Company company) => Create(company);
 
