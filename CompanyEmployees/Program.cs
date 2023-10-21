a501936f3e215360037f4943c5084f3a0019f792
ahmed fathy <ahmd.fathi0074@gmail.com>
2023-10-22 01:44:08 +0300

    add DataShaping in the project

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Options;
 using NLog;
+using Service.DataShaping;
+using Shared.DataTransferObjects;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +26,7 @@ builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.AddScoped<IDataShaper<EmployeeDto>, DataShaper<EmployeeDto>> ();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
